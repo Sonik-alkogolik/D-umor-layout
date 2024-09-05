a0f551d1c04806b8cfb3d031d84317b3b10fa338
BelickijDmitry <belickij@jam-it.ru>
2024-09-05 16:04:53 +0300

    Страница с товарами адаптив

--- index.js
+++ index.js
@@ -24,21 +24,25 @@ document.addEventListener('DOMContentLoaded', function () {
     filterItems.forEach(function(item) {
         item.addEventListener('click', function(e) {
             const target = e.target;
-
             if (target.closest('.filter-name')) {
                 const filterList = this.querySelector('.filter-list');
+                const filterName = this.querySelector('.filter-name');
                 if (filterList) {
                     if (filterList.classList.contains('list_active')) {
                         filterList.classList.remove('list_active');
+                        filterName.classList.remove('active'); 
                     } else {
                         filterItems.forEach(function(i) {
                             const otherFilterList = i.querySelector('.filter-list');
+                            const otherFilterName = i.querySelector('.filter-name');
                             if (otherFilterList) {
                                 otherFilterList.classList.remove('list_active');
+                                otherFilterName.classList.remove('active'); 
                             }
                         });
-
+                     
                         filterList.classList.add('list_active');
+                        filterName.classList.add('active');
                     }
                 }
             }
